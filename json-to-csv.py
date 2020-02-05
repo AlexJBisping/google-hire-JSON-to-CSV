5efb09f97edbcf4443c18a31b5ca737b5dafabcf
Alex Bisping <alex@loxo.co>
2020-02-04 18:53:36 -0700

    Try to process _Hiring files

--- json-to-csv.py
+++ json-to-csv.py
@@ -8,13 +8,18 @@ from pandas.io.json import json_normalize
 directory_path = sys.argv[1]
 json_file_paths = glob.glob('{}/*.json'.format(directory_path))
 for json_file_path in json_file_paths:
-    do_not_parse = ['Hiring_', 'Integrations', 'Historical']
+    json_file_name = os.path.basename(json_file_path)
+    do_not_parse = ['Integrations', 'Historical']
     if any(string in json_file_path for string in do_not_parse):
         continue
-    print 'Processing {}...'.format(os.path.basename(json_file_path))
+    print 'Processing {}...'.format(json_file_name)
     with open(json_file_path) as f:
         json_data = json.load(f)
-    df = json_normalize(json_data)
+    try:
+        df = json_normalize(json_data)
+    except:
+        print('Could not process: {}'.format(json_file_name))
+        continue
     csv_file_path = re.sub(".json", ".csv", json_file_path)
     df.to_csv(csv_file_path, index=False, encoding='utf-8')
 
